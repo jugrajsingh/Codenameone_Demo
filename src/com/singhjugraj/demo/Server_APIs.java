d471120d4aad197632d3b3b7418fcf29e99e18bd
Jugraj SIngh <beatstar101@gmail.com>
2016-07-12 10:16:39 +0530

    Adding Server APIs

---
+++ src/com/singhjugraj/demo/Server_APIs.java
@@ -0,0 +1,12 @@
+package com.singhjugraj.demo;
+
+/**
+ * Created by jugraj on 12/7/16.
+ */
+public class Server_APIs {
+    public static String SERVERURL = "http://localhost";
+    public static String USERDATA = SERVERURL + "/userdata";
+    public static String USERDETAILS = SERVERURL + "/userdetails";
+    public static String NEWUSER = SERVERURL + "/newuser";
+    public static String USERNAME = "ad";
+}
